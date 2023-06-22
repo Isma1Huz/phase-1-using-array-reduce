1c2e2bc01edd6f6d221fe9a3a3923d8a3c5a487c
Isma1Huz <ismael.hussein@student.moringaschool.com>
2023-06-22 23:04:41 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
+
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
 
-    let totalBatteries = 0;
-  
-    for (const product of batteryBatches) {
-        totalBatteries += product
-    };
-  ;
-  
+//     let totalBatteries = 0;
+
+//     for (const product of batteryBatches) {
+//         totalBatteries += product
+//     };
+//   ;
+let totalBatteries = batteryBatches.reduce((f,g) => f + g)
+
   console.log(totalBatteries);
